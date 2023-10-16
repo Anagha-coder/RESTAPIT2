ca166cecaa1853a72084f37e2f42770c10be42d0
Anagha Desai <71918177+Anagha-coder@users.noreply.github.com>
2023-10-16 21:19:56 +0530

    Add files via upload

---
+++ controller/routes.go
@@ -0,0 +1,16 @@
+package controller
+
+import (
+	"empsystem/handlers"
+
+	"github.com/gorilla/mux"
+)
+
+func InitializeRoutes(r *mux.Router) {
+	r.HandleFunc("/employees", handlers.GetAllEmployees).Methods("GET")
+	r.HandleFunc("/employees/{id}", handlers.GetEmployeeByID).Methods("GET")
+	r.HandleFunc("/employees/search", handlers.SearchEmployees).Methods("GET")
+	r.HandleFunc("/employees", handlers.CreateEmployeeHandler).Methods("POST")
+	r.HandleFunc("/employees/{id}", handlers.UpdateEmployeeHandler).Methods("PUT")
+	r.HandleFunc("/employees/{id}", handlers.DeleteEmployeeHandler).Methods("DELETE")
+}
